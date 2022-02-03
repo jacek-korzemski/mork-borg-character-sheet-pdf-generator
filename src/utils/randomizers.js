1e1654e562e75ed28a6d7ff66b4ae40448aa247b
Jacek Korzemski <wrp40.000@wp.pl>
2022-02-03 13:43:18 +0100

    random elements generator testing

--- src/utils/randomizers.js
+++ src/utils/randomizers.js
@@ -1,16 +1,18 @@
-export const randomInt = (min, max) => {
+const randomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export const randomElementFromArray = (array) => {
+const randomElementFromArray = (array) => {
     if (array.length === 0) {
         return "-";
     }
     return array[Math.floor(Math.random() * array.length)];
 };
 
-export const nRandomElementsFromArray = (array, n) => {
+const nRandomElementsFromArray = (array, n) => {
     return [...array].sort(() => 0.5 - Math.random()).slice(0, n);
 };
+
+export { randomInt, randomElementFromArray, nRandomElementsFromArray };
